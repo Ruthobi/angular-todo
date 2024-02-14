9dbe427906410320dd5fb85947700735e1edb98c
Ruthobi <prettychiomaobi9@gmail>
2024-02-14 09:22:12 -0800

    Co-authored-by: Israel Oluwaferanmi Alagbe <ioa@andrew.cmu.edu>

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,56 +1,120 @@
 import { Component, OnInit } from '@angular/core';
-import {Todo} from './../../models/Todo' ;
+import { Todo } from './../../models/Todo';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent implements OnInit{
+export class TodoComponent implements OnInit {
 
-submit(){
-  
-  
-}
+  todos!: Todo[];
+  inputTodo: string ="";
 
-
-todos! : Todo[];
-inputTodo: string ='';
-  constructor(){ }
+  constructor() { }
 
   ngOnInit(): void {
-    this.todos  =[
+    this.todos = [
       {
-        content:'first content',
-        completed:false
+        content: 'first content',
+        completed: false
       },
       {
-        content:'second content',
-        completed:false
+        content: 'second content',
+        completed: false
       },
     ]
   }
-toggleDone (id:number) {
-this.todos.map((v, i) =>{
-  if(i == id) v. completed  = !v. completed;
 
-  return v;
-})
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    })
+  }
+
+  editTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i! == id);
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i! == id);
+  }
+
+  addTodo() {
+    console.log(this.inputTodo)
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = "";
+  }
+
+
+
 }
 
-deleteTodo (id:number) {
-  this.todos  =this.todos.filter((v,i)  => i ! == id);
-}
 
-addTodo(){
-  this.todos.push({
-    content:this.inputTodo,
-    completed:false
-  });
 
-  this.inputTodo ="";
-}
 
-}
+  
+
+
+ 
+ 
+  
+
+
+ 
+  
+
+ 
+
+
+  // addTodo() {
+  //   console.log(this.inputTodo)
+  //   this.todos.push({
+  //     content: this.inputTodo,
+  //     completed: false
+  //   });
+
+  //   this.inputTodo = "";
+  // }
+
+  //   }
+// }
+// editTodo(id: number) {
+//   this.todos = this.todos.filter((v, i) => i! == id);
+// }
+
+// // Function to update a todo item
+// updateTodo() {
+//   if (this.editIndex !== null && this.editContent.trim() !== '') {
+//       this.todos[this.editIndex].content = this.editContent;
+//       this.editIndex = null;
+//       this.editContent = '';
+//   }
+// }
+
+// // Function to delete a todo item
+// deleteTodo(id: number) {
+//   this.todos = this.todos.filter((v, i) => i! == id);
+// }
+
+// // Function to toggle the completion status of a todo item
+// toggleDone(index: number) {
+//   this.todos.map((v, i) => {
+//     if (i == id) v.completed = !v.completed;
+
+//     return v;
+//   })
+// }
+
+
+
+
+
 
 
 
